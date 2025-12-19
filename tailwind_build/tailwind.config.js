8fe1b84d3d289ece43f0fd049b1421eea31e51fe
vertexcreations <vertex.creations.ventas@gmail.com>
2025-12-19 14:28:43 -0400

    feat: Add initial Tailwind CSS setup including configuration and dependencies.

--- tailwind_build/tailwind.config.js
+++ tailwind_build/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./templates/**/*.html", "./static/**/*.js"],
+    content: ["../templates/**/*.html", "../static/**/*.js"],
     theme: {
         extend: {
             colors: {
